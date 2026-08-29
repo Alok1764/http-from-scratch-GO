28e0322b40a49efeb22871970c8664b85d9c3b15
ALOK <alokpal1764@gmail.com>
2026-08-29 16:27:04 +0530

    8 bytes read but with line

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -12,14 +13,21 @@ func main() {
 	if err != nil {
 		log.Fatal("error :", err)
 	}
-
+	st := ""
 	for {
 		data := make([]byte, 8)
 		n, err := f.Read(data)
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+
+		if i := bytes.IndexByte(data, '\n'); i == -1 {
+			st += string(data[:n])
+		} else {
+			st += string(data[:i])
+			fmt.Printf("read: %s\n", st)
+			st = string(data[i+1 : n])
+		}
 
 	}
 
